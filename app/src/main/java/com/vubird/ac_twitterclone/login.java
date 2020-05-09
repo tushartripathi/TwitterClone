a05d4b067810ffef63e33a7afd5b705ac215db84
tushartripathi301997@gmail.com <tushartripathi301997@gmail.com>
2020-05-09 17:40:14 +0530

    1) Log in 2) Sing in 3) Follow & Unfollwo people

---
+++ app/src/main/java/com/vubird/ac_twitterclone/login.java
@@ -0,0 +1,104 @@
+package com.vubird.ac_twitterclone;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.shashank.sony.fancytoastlib.FancyToast;
+
+
+
+public class login extends AppCompatActivity {
+
+    EditText usernameBox, passwordBox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        usernameBox = findViewById(R.id.usernameBox);
+        passwordBox = findViewById(R.id.passwordBox);
+
+        passwordBox.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+
+                if(i==KeyEvent.KEYCODE_ENTER &&  keyEvent.getAction()==KeyEvent.ACTION_DOWN)
+                {
+                    LoginUser(view);
+                }
+                return false;
+            }
+        });
+
+        if(ParseUser.getCurrentUser()!=null)
+        {
+            ParseUser.logOut();
+            Intent i = new Intent(this, userListActivity.class);
+            startActivity(i);
+        }
+    }
+
+    public  void gotoSignIn(View view)
+    {
+        Intent i = new Intent(this, signUp.class);
+        startActivity(i);
+    }
+
+    public void LoginUser(View view)
+    {
+
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Logging In " + usernameBox.getText().toString() + "......... ");
+        progressDialog.show();
+        ParseUser.logInInBackground(usernameBox.getText().toString().trim(), passwordBox.getText().toString().trim(), new LogInCallback() {
+
+            @Override
+            public void done(ParseUser user, ParseException e) {
+
+                if(user!=null && e==null)
+                {
+
+                    FancyToast.makeText(login.this, "Welcome back! " + usernameBox.getText().toString(), FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
+                    progressDialog.dismiss();
+                    gotoWelcome();
+
+                }
+                progressDialog.dismiss();
+                Toast.makeText(login.this,"Worng Credentials", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    public void gotoWelcome()
+    {
+            Intent i = new Intent(this, userListActivity.class);
+            finish();
+            startActivity(i);
+
+    }
+    public  void rootClick(View view)
+    {
+        try {
+
+
+            InputMethodManager inputMethod = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            inputMethod.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
+        catch (Exception e)
+        {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/vubird/ac_twitterclone/userListActivity.java
@@ -0,0 +1,146 @@
+package com.vubird.ac_twitterclone;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CheckedTextView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class userListActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private ListView listView;
+    private ArrayList<String> userArrayList;
+    private ArrayAdapter arrayAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_list);
+
+        if(ParseUser.getCurrentUser()==null)
+        {
+            Intent i = new Intent(this, login.class);
+            startActivity(i);
+        }
+
+        listView = findViewById(R.id.Userlistview);
+        listView.setOnItemClickListener(userListActivity.this);
+        userArrayList = new ArrayList();
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_checked, userArrayList);
+        listView.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE);
+
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        query.whereNotEqualTo("username",ParseUser.getCurrentUser().getUsername());
+        query.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> objects, ParseException e) {
+                if(e==null && objects.size() > 0)
+                {
+                    for(ParseUser user : objects)
+                    {
+                        userArrayList.add(user.getUsername());
+                    }
+                    listView.setAdapter(arrayAdapter);
+                    for(String twitterUser : userArrayList)
+                    {
+              //          if(ParseUser.getCurrentUser().getList("Following")!=null) {
+                            if (ParseUser.getCurrentUser().getList("Following").contains(twitterUser)) {
+                                listView.setItemChecked(userArrayList.indexOf(twitterUser), true);
+                            }
+                //        }
+                    }
+                }
+
+            }
+        });
+
+
+    }
+
+
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
+    {
+        CheckedTextView checkedTextView = (CheckedTextView)view;
+        if(checkedTextView.isChecked())
+        {
+            FancyToast.makeText(this,userArrayList.get(i) + " Followed",Toast.LENGTH_SHORT, FancyToast.SUCCESS,true).show();
+            ParseUser.getCurrentUser().add("Following", userArrayList.get(i));
+
+            }
+        else
+        {
+            ParseUser.getCurrentUser().getList("Following").remove(userArrayList.get(i));
+            List currentUserFollowing = ParseUser.getCurrentUser().getList("Following");
+            ParseUser.getCurrentUser().remove("Following");
+            ParseUser.getCurrentUser().put("Following",currentUserFollowing);
+        }
+
+
+        ParseUser.getCurrentUser().saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e)
+            {
+                    if(e==null)
+                    {
+
+                    }
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    //called when menu is clicked
+    //swtich case to check which menu item is clicked
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+
+        switch (item.getItemId())
+        {
+            case R.id.postImageItem:
+
+                break;
+            case R.id.logOutUser:
+                ParseUser.logOut();
+                finish();
+                Intent i = new Intent(this, login.class);
+                startActivity(i);
+
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
